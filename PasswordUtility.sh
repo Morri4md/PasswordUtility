3dc8e59bdd3df9fd085b60e2a86be22cec5f2642
Morri4md <Morri4md@mail.uc.edu>
2026-02-24 20:14:38 -0500

    Pushing outline

---
+++ PasswordUtility.sh
@@ -0,0 +1,21 @@
+#Start
+
+$password = 
+$length = 
+$output = 
+$complexity = 
+
+
+#Generator of random passwords with lengths of (UserInput) another option of simple or complex password generator
+#-g
+#-o (output file if specified, otherwise output to console)
+#-c
+
+
+#Validator of password user enters and checks if it meets the requirements of a strong password (length, uppercase, lowercase, numbers, special characters)
+#-v
+#-o (output file if specified, otherwise output to console)
+
+#Help menu that explains how to use the options
+
+#Error handling for invalid inputs
